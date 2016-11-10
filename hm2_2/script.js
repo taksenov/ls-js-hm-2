95eae31798c9ee0d394e2348e5ae63c455652a9c
TAksenov <taksenov@gmail.com>
2016-11-11 00:02:42 +0500

    fix hm2_2

--- hm2_2/script.js
+++ hm2_2/script.js
@@ -15,6 +15,7 @@
 
 // Test variables
 var allNumbers  = [1, 2, 4, 5, 6, 7, 8];
+var emptyArray  = [];
 var someNumbers = [1, 2, 'привет', 4, 5, 'loftschool', 6, 7, 8];
 var noNumbers   = ['это', 'массив', 'без', 'чисел'];
 
