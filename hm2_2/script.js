c932647a550c077734bb9201609b445db2bf92bb
TAksenov <taksenov@gmail.com>
2016-11-10 20:17:01 +0500

    hm2_2 -- some fix in script.js

--- hm2_2/script.js
+++ hm2_2/script.js
@@ -14,8 +14,8 @@
 // console.log(isSomeTrue(noNumbers, isNumber)); //вернет false
 
 // Test variables
-var allNumbers  = [1, 2, 4, 5, 6, 7, 8];
-var emptyArray  = [];
+// var allNumbers  = [1, 2, 4, 5, 6, 7, 8];
+var allNumbers  = [1, 2, '4', 5, 6, 7, 8];
 var someNumbers = [1, 2, 'привет', 4, 5, 'loftschool', 6, 7, 8];
 var noNumbers   = ['это', 'массив', 'без', 'чисел'];
 
